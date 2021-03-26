68e4a3b95b202c45eeac7f8689bdd57872611ba8
fariqkr <fariq.k.r@gmail.com>
2021-03-27 00:31:38 +0700

    initial commit

---
+++ app/src/main/java/com/fariq/sqlite/helper/MappingHelper.kt
@@ -0,0 +1,22 @@
+package com.fariq.sqlite.helper
+
+import android.database.Cursor
+import com.fariq.sqlite.Note
+import com.fariq.sqlite.db.DatabaseContract
+
+object MappingHelper {
+ 
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
